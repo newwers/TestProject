5fea85a6a411ea46af9f80ba40c5e2c65ab288e4
zdq <1554263906@qq.com>
2018-11-05 18:17:20 +0800

    修改忽略文件

---
+++ Assets/PaintPlane/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(AAA());
+        }
+    }
+
+    IEnumerator AAA()
+    {
+        int index = 0;
+        float _time = 0f;
+        _time = Time.time;
+        while (Time.time - _time <=1f)
+        {
+
+            index++;
+            yield return null;
+        }
+        print(index);
+        _time = Time.time;
+        index = 0;
+
+    }
+}
