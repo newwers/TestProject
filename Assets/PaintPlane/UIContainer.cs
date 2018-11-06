fce2942708197b717756d93ee7de62b1573fcc0b
zdq <1554263906@qq.com>
2018-11-06 02:49:31 -0800

    画板功能优化

--- Assets/PaintPlane/UIContainer.cs
+++ Assets/PaintPlane/UIContainer.cs
@@ -9,4 +9,16 @@ public class UIContainer : MonoBehaviour {
     public Button ClearAllButton;
     public Text FPSText;
 
+    [Header("画笔宽度")]
+    public Slider PaintWidth_Slider;
+    public Text PaintWidthNumber_Text;
+
+    [Header("点生成的临界值")]
+    public Slider NewPointThreshold_Slider;
+    public Text NewPointthreshold_Text;
+
+    public Color PaintColor;
+
+    public LineRenderTest lineRenderTest;
+
 }
---
+++ Assets/PaintPlane/UIController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIController : MonoBehaviour {
+
+
+    public UIContainer Container;
+
+    private LineRenderTest lineRenderTest;
+
+    private void Start()
+    {
+        lineRenderTest = Container.lineRenderTest;
+        Container.PaintWidth_Slider.onValueChanged.AddListener(OnPaintWidthChange);
+        Container.NewPointThreshold_Slider.onValueChanged.AddListener(OnNewPointSliderChange);
+
+        Container.PaintWidthNumber_Text.text = (lineRenderTest.width * 10f).ToString();
+        Container.NewPointthreshold_Text.text = (lineRenderTest.NewPointThresholdDistance * 10f).ToString();
+    }
+
+    private void OnNewPointSliderChange(float value)
+    {
+        Container.NewPointthreshold_Text.text = value.ToString();
+        lineRenderTest.NewPointThresholdDistance = value * 0.1f;
+    }
+
+    private void OnPaintWidthChange(float value)
+    {
+        lineRenderTest.width = value * 0.1f;
+        Container.PaintWidthNumber_Text.text = value.ToString();
+    }
+}
